b462ec00ef3dfe8ec2beb69e3d7e49d82ddfcbf6
TejeswarMummineni <tejanaidu19@hotmail.com>
2021-01-11 08:35:26 +0530

    index page logic created

---
+++ BussinessLogic/WelcomeBL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BussinessEntites;
+using DataAccessLayer;
+
+namespace BussinessLogic
+{
+    public class WelcomeBL
+    {
+        WelcomeDAL wd = new WelcomeDAL();
+        public List<JobPostingEntites> Welcomepage(string Search)
+        {
+           return wd.Index(Search);
+        }
+    }
+}
---
+++ DataAccessLayer/WelcomeDAL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BussinessEntites;
+using DataAccessLayer.Models;
+
+namespace DataAccessLayer
+{
+    public class WelcomeDAL
+    {
+        OnlineRecruitmentEntities db = new OnlineRecruitmentEntities();
+        public List<JobPostingEntites> Index(string Search)
+        {
+            List<JobPostingEntites> po = new List<JobPostingEntites>();
+            var res = db.JobPostings.Where(x => x.JobType.StartsWith(Search) || Search == null);
+            foreach (var item in res)
+            {
+                po.Add(new JobPostingEntites()
+                {
+                    JobId = item.JobId,
+                    JobName =item.JobName,
+                    JobType=item.JobType,
+                    JobDescription=item.JobDescription,
+                    CompanyId=item.CompanyId,
+                    Location=item.Location,
+                    PostedDate=item.PostedDate,
+                    LastDate=item.LastDate
+                   
+                });
+                
+            }
+            return po;
+        }
+    }
+}
